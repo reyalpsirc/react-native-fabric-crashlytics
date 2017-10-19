1519e9cef844cc88516486317e7e832d280bff1d
Cristiano Santos <cristianomnsantos@gmail.com>
2017-10-19 15:39:00 +0100

    - Added the code to reference react-native-source-maps to get the correct line numbers for JS on iOS. - Changed the Readme so that it specifies what's needed to have this working.

--- README.md
+++ README.md
@@ -4,6 +4,33 @@ react-native-fabric-crashlytics
 
 Reports javascript exceptions in React Native to the Crashlytics server, using the react-native-fabric library.
 
+Setup iOS
+-----
+
+On your iOS project select the right target scheme and then go to "Build Phases" => "Bundle React Native code and images" and replace the shell code by this:
+
+```
+export NODE_BINARY=node
+if [ "${CONFIGURATION}" = "Release" ]
+then ../node_modules/react-native-fabric-crashlytics/release-with-sourcemap.sh
+else ../node_modules/react-native/scripts/react-native-xcode.sh
+fi
+```
+
+Create an empty file named "sourcemap.js" on "PATH_TO_YOUR_PROJECT/ios" and drag and drop it to the project navigator on XCode, just below the "main.jsbundle" file.
+Once that's done, delete the empty file from the folder (but keep the reference on the project)
+
+Setup Android
+-----
+
+Go to the file "PATH_TO_YOUR_PROJECT/android/app/build.gradle" and change `project.ext.react` to have `extraPackagerArgs` or, if you don't have `project.ext.react`, create it like below:
+
+```
+project.ext.react = [
+  extraPackagerArgs: [ "--sourcemap-output", "$buildDir/intermediates/assets/release/sourcemap.js" ]
+]
+```
+
 Usage
 -----
 
@@ -12,5 +39,9 @@ To use, add this code to your index.ios.js and index.android.js (or some library
 ```
 // Already assumes that Fabric is initialized/configured properly in the iOS and Android app startup code.
 import crashlytics from 'react-native-fabric-crashlytics';
-crashlytics.init();
+function crashload () {
+  const path = `sourcemap.js`
+  crashlytics.init(path)
+}
+crashload()
 ```
--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 import { Platform } from 'react-native';
 import StackTrace from 'stacktrace-js';
+import {initSourceMaps, getStackTrace} from 'react-native-source-maps';
 import { Crashlytics } from 'react-native-fabric';
 var assign = require('lodash.assign');
 
-function init() {
+function init(sourcemapName = null) {
   if (__DEV__) {
     // Don't send exceptions from __DEV__, it's way too noisy!
     // Live reloading and hot reloading in particular lead to tons of noise...
@@ -13,25 +14,56 @@ function init() {
   }
 
   var originalHandler = global.ErrorUtils.getGlobalHandler();
-  function errorHandler(e, isFatal) {
-    StackTrace.fromError(e, {offline: true}).then((x) => {
-      Crashlytics.recordCustomExceptionName(e.message, e.message, x.map((row) => (assign({}, row, {
-        fileName: `${row.fileName}:${row.lineNumber || 0}:${row.columnNumber || 0}`,
-      }))))
-    });
-    // And then re-throw the exception with the original handler
-    if (originalHandler) {
-      if (Platform.OS === 'ios') {
-        originalHandler(e, isFatal);
-      } else {
-        // On Android, throwing the original exception immediately results in the
-        // recordCustomExceptionName() not finishing before the app crashes and therefore not logged
-        // Add a delay to give it time to log the custom JS exception before crashing the app.
-        // The user facing effect of this delay is that separate JS errors will appear as separate
-        // issues in the Crashlytics dashboard.
+  if (sourcemapName) {
+    initSourceMaps({sourceMapBundle: sourcemapName});
+  }
+  async function errorHandler(e, isFatal) {
+    if (sourcemapName) {
+      // map the JS stacktrace using the sourcemap file given on init
+      let jsParsed = false
+      try {
+        e.stack = await getStackTrace(e);
+        if (e.stack) {
+          Crashlytics.recordCustomExceptionName(e.message, e.message, e.stack)
+          jsParsed = true
+        }
+      } catch (error) {
+        // ignore and use the normal stack on the error
+      }
+      // And then re-throw the exception with the original handler
+      if (originalHandler) {
+        // If we want the App to actually exit on a fatal crash, we need to call the originalHandler with true as the 2nd arg
+        // This will create a Fatal error with Non JS stacktrace but still, the JS trace will appear as Non fatal before
+        // We also delay it a bit to give time for the custom exception to be sent
         setTimeout(() => {
+          if (isFatal && jsParsed) {
+            originalHandler(new Error('JS crash happened. Check Fabric dashboard for non fatal errors'), true);
+          } else {
+            originalHandler(e, isFatal);
+          }
+        }, 1000);
+      }
+    } else {
+      // record the exception with the info that we have
+      StackTrace.fromError(e, {offline: true}).then((x) => {
+        Crashlytics.recordCustomExceptionName(e.message, e.message, x.map((row) => (assign({}, row, {
+          fileName: `${row.fileName}:${row.lineNumber || 0}:${row.columnNumber || 0}`,
+        }))))
+      });
+      // And then re-throw the exception with the original handler
+      if (originalHandler) {
+        if (Platform.OS === 'ios') {
           originalHandler(e, isFatal);
-        }, 500);
+        } else {
+          // On Android, throwing the original exception immediately results in the
+          // recordCustomExceptionName() not finishing before the app crashes and therefore not logged
+          // Add a delay to give it time to log the custom JS exception before crashing the app.
+          // The user facing effect of this delay is that separate JS errors will appear as separate
+          // issues in the Crashlytics dashboard.
+          setTimeout(() => {
+            originalHandler(e, isFatal);
+          }, 500);
+        }
       }
     }
   }
